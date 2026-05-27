4dbb8dbd8e516b55dcc54b7462138a49d1c18a5b
Ha Phu <haphu0507@gmail.com>
2026-05-27 23:49:25 +0700

    feat: enhance user model to include gender and date of birth, update related classes and database interactions

--- auction-project/shared/src/main/java/com/auction/model/Admin.java
+++ auction-project/shared/src/main/java/com/auction/model/Admin.java
@@ -6,14 +6,14 @@ import com.auction.enums.UserRole;
 public class Admin extends User {
 
     // Constructor cho tạo mới Admin (thường tạo qua script hoặc hệ thống nội bộ)
-    public Admin(String username, String passwordHash, String email, String fullName) {
-        super(username, passwordHash, email, fullName, UserRole.ADMIN);
+    public Admin(String username, String passwordHash, String email, String fullName, String gender, String dateOfBirth) {
+        super(username, passwordHash, email, fullName, UserRole.ADMIN, gender, dateOfBirth);
     }
 
     // Constructor nạp từ MySQL
     public Admin(String id, LocalDateTime createdAt, LocalDateTime updateAt, String username, 
-                 String passwordHash, String email, String fullName, boolean active) {
-        super(id, createdAt, updateAt, username, passwordHash, email, fullName, UserRole.ADMIN, active);
+                 String passwordHash, String email, String fullName, boolean active, String gender, String dateOfBirth) {
+        super(id, createdAt, updateAt, username, passwordHash, email, fullName, UserRole.ADMIN, active, gender, dateOfBirth);
     }
 
     @Override
